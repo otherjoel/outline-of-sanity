3fedaf322adf79a081c8b518e63bab37b427844d
Joel Dueck <joel@jdueck.net>
2019-10-02 22:49:58 -0500

    Add colophon

---
+++ colophon.poly.pm
@@ -0,0 +1,8 @@
+#lang pollen
+
+◊(define-meta level "colophon")
+◊(define-meta part "◊ COLOPHON")
+
+This online edition of ◊i{The Outline of Sanity} is adapted from the plain text copy at ◊a[#:href "http://www.gkc.org.uk/gkc/books/Sanity.txt"]{◊tt{gkc.org.uk}}. Published with ◊a[#:href "https://pollenpub.com"]{Pollen}. The source code is ◊a[#:href "https://github.com/otherjoel/outline-of-sanity"]{on GitHub}.
+
+Many of us have a love/hate relationship with Chesterton. He is annoyingly precious, his flourishes frequently obscure his points rather than illuminate them, and his analogies are usually overloaded and contrived. This book in particular would benefit from a careful translation into the English language. But despite its ugliness, it contains ideas about autonomy, ownership and decentralization that break across typical “left-right” fault lines, and which have been tried with success (◊a[#:href "https://en.wikipedia.org/wiki/Distributism#Antitrust_legislation"]{e.g.}). These ideas can still shine through to a persistent reader. I thought it was worth having them online in a form where we can reference and share them easily.
