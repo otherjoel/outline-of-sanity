e7a73ad7ecc5af3e720351cd3a017a0c67af034c
Joel Dueck <joel@jdueck.net>
2019-10-04 14:02:49 -0500

    oooh nice

--- about.poly.pm
+++ about.poly.pm
@@ -3,6 +3,6 @@
 ◊(define-meta level "about")
 ◊(define-meta part "◊ ABOUT")
 
-This online edition of ◊i{The Outline of Sanity} is adapted from the plain text copy at ◊a[#:href "http://www.gkc.org.uk/gkc/books/Sanity.txt"]{◊code{gkc.org.uk}}. Published with ◊a[#:href "https://pollenpub.com"]{Pollen}. The source code is ◊a[#:href "https://github.com/otherjoel/outline-of-sanity"]{on GitHub}.
+This online edition of ◊i{The Outline of Sanity} is adapted from the plain text copy at ◊a[#:href "http://www.gkc.org.uk/gkc/books/Sanity.txt"]{◊code{gkc.org.uk}}. Published with ◊a[#:href "https://pollenpub.com"]{Pollen}. The source code is ◊a[#:href "https://github.com/otherjoel/outline-of-sanity"]{on GitHub}. No trackers, analytics or JavaScript.
 
 Many of us have a love/hate relationship with Chesterton. He is annoyingly precious, his flourishes frequently obscure his points rather than illuminate them, and his analogies are usually overloaded and contrived. This book in particular would benefit from a careful translation into the English language. But despite its ugliness, it contains ideas about autonomy, ownership and decentralization that break across typical “left-right” fault lines, and which have been tried with success (◊a[#:href "https://en.wikipedia.org/wiki/Distributism#Antitrust_legislation"]{e.g.}). These ideas can still shine through to a persistent reader. I thought it was worth having them online in a form where we can reference and share them easily.
